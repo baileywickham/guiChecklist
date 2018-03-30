52c381f898507d19f52ea98228f43b21964bf8a3
b <baileywickham@gmail.com>
2018-03-30 11:35:14 -0700

    changes

--- checklist.py
+++ checklist.py
@@ -1,21 +1,42 @@
 import pymysql.cursors
 import asyncio
-connection = pymysql.connect(host='localhost', user='root', password='planes', db='checklist', cursorclass=pymysql.cursors.DictCursor)
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='planes',
+                             db='checklist',
+                             cursorclass=pymysql.cursors.DictCursor)
 
+# All db connections will be enclosed in with statements to handle closing properly.
+# Avoid global variable
+colorDict = {'blue' : '\033[94m', 'endc' : '\033[0m'}
+def update(ordernum):
+    with connection.cursor() as cursor:
+        cursor.execute('select * from orders where ordernum=' + ordernum) 
+        orderDict = cursor.fetchone()
+        assert orderDict is not None, "Order number does not exist"
+        printer(orderDict) 
+
+def printer(orderDict): 
+    for x in sorted(orderDict):
+        print(x, ":", orderDict.get(x))
+
+def lastUpdatedDate(startUp=False):
+    if startUp:
+        with connection.cursor() as cursor:
+            cursor.execute('show table status from checklist like \'orders\'')
+            lastUpdateOfDatabase = cursor.fetchone().get("Update_time") 
+    def newDate():
+        with connection.cursor() as cursor:
+            cursor.execute('show table status from checklist like \'orders\'')
+            dt = cursor.fetchone().get("Update_time")
+            if dt > lastUpdateOfDatabase:
+                update()
+                lastUpdatedDate = dt
+            else:
+                print("current updated date is current")
 def main():
-    try:
-       with connection.cursor() as cursor:
-            sql = 'select * from orders where ordernum=' + input("Enter order ID: ")
-            cursor.execute(sql)
-            # Handle gracefully in production
-            assert cursor.fetchone() is not None, "order number does not exist"
-            print(cursor.fetchone())
-    finally:
-        cursor.close()
+    lastUpdatedDate(True)
+    update(input("Enter an ordernumber: "))
 
-def update():
-   
-def printer():
-
-main()
- 
+if __name__ == '__main__':
+    main()
--- test.py
+++ test.py
@@ -1,17 +1,26 @@
 import pymysql.cursors
-connection = pymysql.connect(host='localhost', user='root', password='planres', db='checklist', cursorclass=pymysql.cursor.DictCursor)
+from datetime import datetime
+connection = pymysql.connect(host='localhost', user='root', password='planes', db='checklist', cursorclass=pymysql.cursors.DictCursor)
+
+
 def lastUpdatedDate(date): 
-    lastUpdateofDatabase = date 
-    def newDate(newDate):
-         if newDate > date:
-             print("new date is newer")
-             # call update function
-             return lastupdate = lastUpdatedDate() 	
-
-
+    if date is not None:
+        lastUpdateofDatabase = date 
+    def newDate():
+        with connection.cursor() as cursor:
+            cursor.execute('show table status from checklist lile \'orders\'')
+            dt = cursor.fetchone().get("Update_time")	
+            if dt > lastUpdatedDate:
+                updateList()
+                lastUpdatedDate = dt 
+            else:
+                print("current updated date is current")
 def main():
-    lastUpdatedDate1 = lastUpdatedDate(date)
-    if lastUpdatedDate1():
-        lastUpdatedDate1 = lastUpdatedDate() 
+    with connection.cursor() as cursor:
+
+        sql = 'show table status from checklist like \'orders\''  
+        cursor.execute(sql)
+        dt = datetime.strptime(cursor.fetchone().get("Update_time"), '%Y-%m-%d %H:%M:%S')
 # The goal here is to have a function that checks when the last update to the database was,
 # and if ]the newest update is sooner than the old update, cal
+main()
